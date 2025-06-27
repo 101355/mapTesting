1b52bef2351592faa1561fd3ba9a3207139fc187
Nyan Lin <nyan101355@gmail.com>
2025-06-28 02:30:57 +0630

    mkmk

--- src/App.vue
+++ src/App.vue
@@ -207,9 +207,9 @@ async function updateRoute() {
     const distanceInKm = route.distance / 1000;
 
     let minutesPerKm = 1; // default for driving
-    if (travelMode.value === 'walking') minutesPerKm = 14;
+    if (travelMode.value === 'walking') minutesPerKm = 13;
     else if (travelMode.value === 'cycling') minutesPerKm = 6;
-    else if (travelMode.value === 'driving') minutesPerKm = 1.8;
+    else if (travelMode.value === 'driving') minutesPerKm = 2;
 
     // Calculate duration in seconds (minutes per km * 60 * distance in km)
     currentDuration.value = minutesPerKm * 60 * distanceInKm;
